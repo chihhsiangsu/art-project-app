468795f672a73a4c50caeacc6bdb1c8856233819
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-14 23:10:07 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Piece of The Day</h1>
       <SpotLightPiece />
       <Link href="art-pieces">Art Gallery</Link>
     </div>
